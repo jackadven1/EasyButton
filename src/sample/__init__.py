a97c4316fbc7a94228119e5ef87c085acaaec600
William Halsted <76800438+jackadven1@users.noreply.github.com>
2020-12-31 16:14:20 -0800

    Update __init__.py

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,6 +1,5 @@
+from pybutton import all *
+from examples import all*
 '''def main():
     #Entry point for the application script
     #print("Call your main application code here")'''
-
-import pybutton
-return
